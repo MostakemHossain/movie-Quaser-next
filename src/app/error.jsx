b3a7758ea8159a217a22461370f4d4ae29846271
MostakemHossain <mostakememon123@gmail.com>
2024-03-02 07:55:25 +0600

    some modification

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -2,16 +2,17 @@
 
 import { useEffect } from "react";
 
-export default function error({ error, reset }) {
-    useEffect(()=>{
-        console.log(error)
-    },[error])
-  return (
-    <div className='text-center mt-10'>
-      <h1>Something went wrong. Please try again later.</h1>
-      <button className='hover:text-amber-600' onClick={() => reset()}>
-        Try Again
-      </button>
-    </div>
-  );
+export default function Error({ error, reset }) {
+    useEffect(() => {
+        console.log(error);
+    }, [error]);
+
+    return (
+        <div className='text-center mt-10'>
+            <h1>Something went wrong. Please try again later.</h1>
+            <button className='hover:text-amber-600' onClick={() => reset()}>
+                Try Again
+            </button>
+        </div>
+    );
 }
